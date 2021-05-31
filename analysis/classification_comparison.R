173dd0c6541bf6180f028eeac137a5cd16ef7f3f
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-05-31 10:55:29 +1000

    Comparative classification

---
+++ analysis/classification_comparison.R
@@ -0,0 +1,20 @@
+#-------------------------------------
+# This script sets out to produce
+#analysis of classification performance
+# by problem and feature set
+#
+# NOTE: setup.R and fit_classifiers.R 
+# must be run first
+#-------------------------------------
+
+#-------------------------------------
+# Author: Trent Henderson, 31 May 2021
+#-------------------------------------
+
+# Read in classification results
+
+load("data/classifierOutputs.Rda")
+
+# Produce plot
+
+
