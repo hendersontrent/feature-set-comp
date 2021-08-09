29a376f269590600854981feaa787d2d77c08134
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-08-09 11:47:00 +1000

    Empirical 100 webscraped

--- webscraping/pull_empirical_1000.R
+++ webscraping/pull_empirical_1000.R
@@ -13,7 +13,7 @@
 #' Function to automatically webscrape and process the Empirical 1000 datasets
 #' 
 #' 
-#' @return a list object with each of the problems as a dataframe
+#' @return a dataframe with the datasets organised in a tidy way
 #' @author Trent Henderson
 #' 
 
@@ -21,6 +21,29 @@ pull_empirical_1000 <- function(){
   
   message("Downloading data... This may take a long time as the file is large.")
   
+  # Data
+  
+  temp <- tempfile()
+  download.file("https://ndownloader.figshare.com/files/24950795",temp)
+  ts <- read.csv(temp, header = FALSE)
+  
+  ts <- ts %>%
+    mutate(id = dplyr::row_number()) %>%
+    pivot_longer(!id, names_to = "timepoint", values_to = "value") %>%
+    mutate(timepoint = as.numeric(gsub("V", "\\1", timepoint)))
+  
+  # Information file
+  
+  temp1 <- tempfile()
+  download.file("https://ndownloader.figshare.com/files/24950798",temp1)
+  ts_info <- read.csv(temp1, header = TRUE)
+  
+  # Merge
+  
+  main <- ts %>%
+    dplyr::left_join(ts_info, by = c("id" = "ID"))
+  
+  return(main)
 }
 
 # Run function and save output
