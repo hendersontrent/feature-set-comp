b03721a424a74d4d1e0e016c2f7c20858198bd67
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-12 23:26:11 +1000

    Refactored utility functions

--- analysis/redundancy/pca.R
+++ analysis/redundancy/pca.R
@@ -36,52 +36,7 @@ rm(Emp1000FeatMat, hctsa)
 # successfully completed
 #------------------------
 
-#' Function to find datasets that successfully computed
-#' 
-#' @param the_set the feature set to focus on
-#' @return an object of class vector
-#' @author Trent Henderson
-#' 
-
-get_computed_ids <- function(the_set){
-  
-  tmp <- fullFeatMat %>%
-    filter(method == the_set) %>%
-    drop_na() %>%
-    dplyr::select(c(id)) %>%
-    distinct() %>%
-    pull()
-  
-  return(tmp)
-}
-
-#' Function to retain only the consistent time series across feature sets
-#' 
-#' @return an object of class vector
-#' @author Trent Henderson
-#' 
-
-get_consistent_datasets <- function(){
-  
-  catch22 <- get_computed_ids(the_set = "catch22")
-  feasts <- get_computed_ids(the_set = "feasts")
-  tsfeatures <- get_computed_ids(the_set = "tsfeatures")
-  Kats <- get_computed_ids(the_set = "Kats")
-  tsfresh <- get_computed_ids(the_set = "tsfresh")
-  TSFEL <- get_computed_ids(the_set = "TSFEL")
-  hctsa <- get_computed_ids(the_set = "hctsa")
-  
-  tst <- c(catch22, feasts, tsfeatures, Kats, tsfresh, TSFEL, hctsa)
-  
-  tst <- intersect(catch22, feasts)
-  tst <- intersect(tst, tsfeatures)
-  tst <- intersect(tst, Kats)
-  tst <- intersect(tst, tsfresh)
-  tst <- intersect(tst, TSFEL)
-  tst <- intersect(tst, hctsa)
-  
-  return(tst)
-}
+source("R/utility_functions.R")
 
 good_datasets <- get_consistent_datasets()
 
