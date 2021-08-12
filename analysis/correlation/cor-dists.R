527931d43908bb345b5008d1ebbcc1c209601673
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-12 14:43:41 +1000

    Correlation structure skeleton

---
+++ analysis/correlation/cor-dists.R
@@ -0,0 +1,19 @@
+#------------------------------------
+# This script sets out to calculate
+# the distribution of feature
+# correlations between sets
+#
+# NOTE: setup.R must be run first
+#------------------------------------
+
+#----------------------------------------
+# Author: Trent Henderson, 12 August 2021
+#----------------------------------------
+
+# Load feature matrix
+
+load("data/Emp1000FeatMat.Rda")
+
+#-------------- XXX ------------------
+
+
