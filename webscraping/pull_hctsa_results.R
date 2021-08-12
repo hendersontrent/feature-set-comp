c621e62f14fb149f543e8871fdbfa21dbeff79ee
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-12 18:11:13 +1000

    Correct URLs

--- webscraping/pull_hctsa_results.R
+++ webscraping/pull_hctsa_results.R
@@ -26,19 +26,23 @@ pull_hctsa_results <- function(){
   
   temp <- tempfile()
   download.file("https://ndownloader.figshare.com/files/29061867", temp, mode = "wb")
-  d <- readr::read_csv(temp, col_names = FALSE)
+  data_matrix <- readr::read_csv(temp, col_names = FALSE)
   
   #-------------
   # PART II: IDs
   #-------------
   
-  
+  temp2 <- tempfile()
+  download.file("https://ndownloader.figshare.com/files/29061879", temp2, mode = "wb")
+  ids <- readr::read_csv(temp2, col_names = FALSE)
   
   #-------------------
   # PART III: FEATURES
   #-------------------
   
-  
+  temp3 <- tempfile()
+  download.file("https://ndownloader.figshare.com/files/29061870", temp3, mode = "wb")
+  features <- readr::read_csv(temp3, col_names = FALSE)
   
   # Merge all together
   
