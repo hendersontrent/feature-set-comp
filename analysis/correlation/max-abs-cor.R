f9fe6dc8aa0fbc99b594daaf5f6b9e512e16e3f5
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-17 17:15:53 +1000

    Max abs cor script

---
+++ analysis/correlation/max-abs-cor.R
@@ -0,0 +1,13 @@
+#--------------------------------------
+# This script sets out to produce
+# a matrix visualisation of the max abs
+# correlation between feature sets
+#
+# NOTE: setup.R must be run first
+#--------------------------------------
+
+#----------------------------------------
+# Author: Trent Henderson, 13 August 2021
+#----------------------------------------
+
+
