8bde4c1d7daac79a1958843d8931120adb5eae14
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-20 18:14:28 +1000

    More comp time refactoring

--- analysis/comp-time/benchmarking.R
+++ analysis/comp-time/benchmarking.R
@@ -1,8 +1,6 @@
 #--------------------------------------
 # This script sets out to produce
-# computation time benchmarking for
-# all feature sets on the datasets that
-# all successfully compute features on
+# computation time benchmarking analysis
 #
 # NOTE: setup.R must be run first
 #--------------------------------------
@@ -11,107 +9,49 @@
 # Author: Trent Henderson, 17 August 2021
 #----------------------------------------
 
-# Read in data
-
-load("data/empirical1000.Rda")
-
 # Fix Python environment
 
 reticulate::use_python("~/opt/anaconda3/bin/python", required = TRUE)
 
-# Define a helper function
+#------------------ Benchmark calculations ----------
 
-#' Function to compute benchmarks
-#' 
-#' @param my_set the feature set to calculate
-#' @return an object of class dataframe
-#' @author Trent Henderson
-#' 
+#-------------------
+# Compute times
+# for R packages
+#-------------------
 
-run_benchmark <- function(my_set){
-  
-  set.seed(123)
-  ids <- unique(empirical1000$id)
-  storage <- list()
-  
-  # Random one off to generate burn-in
-  
-  short_id <- ids[1]
-  
-  shorter <- empirical1000 %>% 
-    filter(id == short_id)
-  
-  calculate_features(shorter, id_var = "id", 
-                     time_var = "timepoint", 
-                     values_var = "value", 
-                     group_var = "Keywords", 
-                     feature_set = my_set)
 
-  for(i in ids){
-    
-    message(paste0("Computing features for ", i))
-    
-    tryCatch({
-    
-      tmp <- empirical1000 %>% 
-        filter(id == i)
-      
-      m <- summary(microbenchmark(calculate_features(tmp, 
-                                                     id_var = "id", 
-                                                     time_var = "timepoint", 
-                                                     values_var = "value", 
-                                                     group_var = NULL, 
-                                                     feature_set = my_set), 
-                                  times = 1, unit = "s"))
-      
-      mdat <- data.frame(m) %>%
-        dplyr::select(c(mean)) %>%
-        mutate(id = i)
-    
-    }, error = function(e){cat("ERROR :",conditionMessage(e), "\n")})
-    
-    storage[[i]] <- mdat
-  }
-  
-  outs <- data.table::rbindlist(storage, use.names = TRUE) %>%
-    mutate(feature_set = my_set)
-  
-  return(outs)
-}
 
-# Run benchmarking
+#-------------------
+# Read in results
+# for non-R packages
+#-------------------
 
-outs_22 <- run_benchmark(my_set = "catch22")
-outs_feasts <- run_benchmark(my_set = "feasts")
-outs_tsfeatures <- run_benchmark(my_set = "tsfeatures")
-outs_kats <- run_benchmark(my_set = "kats")
-outs_tsfresh <- run_benchmark(my_set = "tsfresh")
-outs_tsfel <- run_benchmark(my_set = "tsfel")
 
-all_features <- bind_rows(outs_22, outs_feasts, outs_tsfeatures, outs_kats, outs_tsfresh, outs_tsfel)
 
-# Join in length labels
+#-------------------
+# Bind all together
+# for plotting
+#-------------------
 
-length_labels <- empirical1000 %>%
-  group_by(id) %>%
-  summarise(length = max(timepoint)) %>%
-  ungroup()
 
-all_features2 <- all_features %>%
-  left_join(length_labels, by = c("id" = "id")) %>%
-  group_by(feature_set, length) %>%
-  summarise(mean = mean(mean)) %>%
-  ungroup()
 
 #------------------ Graphical summary ---------------
 
-p <- all_features2 %>%
+p <- all_features %>%
   ggplot() +
-  geom_point(aes(x = length, y = mean, colour = feature_set)) +
+  geom_errorbar(aes(x = ts_length, ymin = min, ymax = max, colour = feature_set), width = 0.1) +
+  geom_line(aes(x = ts_length, y = mean, colour = feature_set)) +
+  geom_point(aes(x = ts_length, y = mean, colour = feature_set), size = 2) +
   labs(x = "Time Series Length",
        y = "Computation Time (s)",
        colour = NULL) +
   scale_colour_brewer(palette = "Dark2") +
+  scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x, n = 3),
+                labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+  scale_y_log10(limits = c(1e-3, 1e3),
+                breaks = scales::trans_breaks("log10", function(x) 10^x, n = 7),
+                labels = scales::trans_format("log10", scales::math_format(10^.x))) +
   theme_bw() +
   theme(panel.grid.minor = element_blank(),
         legend.position = "bottom")
@@ -120,21 +60,3 @@ print(p)
 
 ggsave("output/comp-time.png", p)
 ggsave("output/comp-time.svg", p)
-
-#------------------ Numerical summary ---------------
-
-# Get number of features per set
-
-load("data/Emp1000FeatMat.Rda") # Load feature matrix
-
-num_feats <- Emp1000FeatMat %>%
-  dplyr::select(c(id, names, method)) %>%
-  distinct() %>%
-  group_by(id, names, method) %>%
-  summarise(counter = n()) %>%
-  ungroup() %>%
-  dplyr::select(c(names, method)) %>%
-  distinct() %>%
-  group_by(method) %>%
-  summarise(counter = n()) %>%
-  ungroup()
---
+++ analysis/comp-time/generate_processes.R
@@ -0,0 +1,62 @@
+#--------------------------------------
+# This script sets out to simulate
+# synthetic time series to use as the
+# basis for computation time analysis
+#
+# NOTE: setup.R must be run first
+#--------------------------------------
+
+#----------------------------------------
+# Author: Trent Henderson, 20 August 2021
+#----------------------------------------
+
+#' Helper function to generate synthetic data
+#' 
+#' @param n the length of the time series to generate
+#' @param nsims the number of time series of length n to generate
+#' @return an object of class dataframe
+#' @author Trent Henderson
+#' 
+
+generate_processes <- function(n, nsims = 5){
+  
+  set.seed(123)
+  
+  # Normal version
+  
+  for(i in 1:nsims){
+    
+    tmp <- data.frame(values = c(rnorm(n, mean = 0, sd = 1)))
+    
+    write.csv(tmp, paste0("data/sims/",n,"_",i,".csv"))
+  }
+  
+  # Add datetime for Kats
+  
+  for(i in 1:nsims){
+    
+    tmp <- data.frame(value = c(rnorm(n, mean = 0, sd = 1))) %>%
+      mutate(timepoint = row_number())
+    
+    unique_times <- unique(tmp$timepoint)
+    
+    datetimes <- data.frame(timepoint = unique_times) %>%
+      dplyr::mutate(time = seq(as.Date("1800-01-01"), by = "day", length.out = length(unique_times)))
+    
+    # Join in datetimes
+    
+    tmp2 <- tmp %>%
+      dplyr::left_join(datetimes, by = c("timepoint" = "timepoint")) %>%
+      dplyr::select(c(time, value))
+    
+    write.csv(tmp2, paste0("data/sims/kats/",n,"_",i,".csv"))
+  }
+}
+
+# Generate the data
+
+generate_processes(n = 100, nsims = 5)
+generate_processes(n = 250, nsims = 5)
+generate_processes(n = 500, nsims = 5)
+generate_processes(n = 750, nsims = 5)
+generate_processes(n = 1000, nsims = 5)
