7cdc5a3e09ef7f635c0fad5e6abc20a3db213022
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-20 22:26:29 +1000

    TSFEL now computes

--- analysis/comp-time/tsfel_comptime.py
+++ analysis/comp-time/tsfel_comptime.py
@@ -8,14 +8,41 @@
 # Author: Trent Henderson, 20 August 2021
 #----------------------------------------
 
-# Read in time series data
+import glob
+import os
+import time
+import pandas as pd
+import numpy as np
+import tsfel
 
+#------------ Run benchmarking -------------
 
+os.chdir("/Users/trenthenderson/Documents/Git/feature-set-comp/data/sims")
 
-# Run benchmarking
+cols = ['ts_length', 'mean', 'feature_set']
+lst = []
 
+for file in glob.glob("*.csv"):
 
+    # Read in file
+
+    x = pd.read_csv(file)
+    x = x.loc[:,"values"]
+
+    # Run computation
+
+    if __name__ == '__main__': 
+        cfg_file = tsfel.get_features_by_domain()
+        start = time.time()
+        extracted_features = tsfel.time_series_features_extractor(cfg_file, x)
+        stop = time.time()
+        duration = stop - start
+        lst.append([x.shape[0], duration, "TSFEL"])
+
+# Get results in clean tidy format
+
+results = pd.DataFrame(lst, columns = cols)
 
 # Store outputs
 
-
+results.to_csv("/Users/trenthenderson/Documents/Git/feature-set-comp/output/comptime/tsfel.csv", index = False)
