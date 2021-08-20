e2a3021fc895de1e51750d9d76c44e15d6312094
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-20 22:30:25 +1000

    Kats fixes

--- analysis/comp-time/kats_comptime.py
+++ analysis/comp-time/kats_comptime.py
@@ -8,14 +8,47 @@
 # Author: Trent Henderson, 20 August 2021
 #----------------------------------------
 
-# Read in time series data
+import glob
+import os
+import time
+import pandas as pd
+import numpy as np
+from kats.consts import TimeSeriesData
+from kats.tsfeatures.tsfeatures import TsFeatures
 
+#------------ Run benchmarking -------------
 
+os.chdir("/Users/trenthenderson/Documents/Git/feature-set-comp/data/sims/kats")
 
-# Run benchmarking
+cols = ['ts_length', 'mean', 'feature_set']
+lst = []
 
+for file in glob.glob("*.csv"):
 
+    # Read in file
+
+    data = pd.read_csv(file)
+    data['time'] = pd.to_datetime(data['time'])
+    data['time'] = [x.date() for x in data.time]
+    data = TimeSeriesData(data)
+
+    # Instantiate TsFeatures
+
+    model = TsFeatures()
+
+    # Run computation
+
+    if __name__ == '__main__': 
+        start = time.time()
+        extracted_features = model.transform(data)
+        stop = time.time()
+        duration = stop - start
+        lst.append([data.shape[0], duration, "tsfresh"])
+
+# Get results in clean tidy format
+
+results = pd.DataFrame(lst, columns = cols)
 
 # Store outputs
 
-
+results.to_csv("/Users/trenthenderson/Documents/Git/feature-set-comp/output/comptime/kats.csv", index = False)
