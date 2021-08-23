c3630e32c091e0e04b75388365aa206778a6e37b
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-23 19:25:22 +1000

    hctsa script

--- analysis/feature-calcs/hctsa.m
+++ analysis/feature-calcs/hctsa.m
@@ -2,4 +2,6 @@
 
 TS_Init('/Users/trenthenderson/Documents/Git/feature-set-comp/data/empirical1000.mat','INP_mops.txt','INP_ops.txt');
 
-% 
+% Run main computation using multiple cores in parallel
+
+TS_Compute(true);
